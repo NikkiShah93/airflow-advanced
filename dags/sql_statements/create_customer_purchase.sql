8b4ba7343befe6c37174d3118ad9303c725e7a5a
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-09-24 10:46:43 -0400

    insert statements

--- dags/sql_statements/create_customer_purchase.sql
+++ dags/sql_statements/create_customer_purchase.sql
@@ -1,7 +1,6 @@
-USE SALES;
 CREATE TABLE IF NOT EXISTS customer_purchase (
     ID INT PRIMARY KEY,
     product VARCHAR(100) NOT NULL,
     price INT NOT NULL,
-    customer_id NOT NULL REFERENCES customer (id)
+    customer_id INT NOT NULL REFERENCES customers (id)
 );

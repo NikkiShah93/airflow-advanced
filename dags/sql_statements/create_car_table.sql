c1965799781857d908840528b8f6f9362621ec64
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-09-25 12:22:23 -0400

    create table statement

---
+++ dags/sql_statements/create_car_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS car_data (
+    id INTEGER PRIMARY KEY,
+    brand TEXT NOT NULL,
+    model TEXT NOT NULL,
+    body_style TEXT NOT NULL,
+    seat INTEGER NOT NULL,
+    price INTEGER NOT NULL);

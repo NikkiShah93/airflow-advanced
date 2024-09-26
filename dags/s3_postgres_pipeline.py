cb7c10871325ca0dafdacfe2d8e6456cd7607a8e
Negar Shahrokh <neg.shahrokh@gmail.com>
2024-09-26 11:03:06 -0400

    fixed the insertion issue

--- dags/s3_postgres_pipeline.py
+++ dags/s3_postgres_pipeline.py
@@ -21,6 +21,7 @@ pg_car_conn = 'PG_CONNECTION_CAR'
 bucket_name = Variable.get('BUCKET_NAME')
 file_key = Variable.get('FILE_KEY')
 cols = ['Brand','Model','BodyStyle','Seats','PriceEuro']
+table_cols = ['brand','model','body_style','seat','price']
 
 default_args = {
     'owner':'nshk'
@@ -41,6 +42,11 @@ def s3_postgres_pipeline_api():
     #     autocommit=True,
     #     sql='CREATE DATABASE car_db;'
     # )
+    drop_table = PostgresOperator(
+        task_id='drop_table',
+        postgres_conn_id=pg_car_conn,
+        sql='DROP TABLE IF EXISTS car_data;'
+    )
     create_table = PostgresOperator(
         task_id='create_table',
         postgres_conn_id=pg_car_conn,
@@ -57,36 +63,39 @@ def s3_postgres_pipeline_api():
         with open(f'{data_path}/s3_file.csv', 'w') as f:
             f.writelines(obj)
 
-        return {'file_path':f'{data_path}/s3_file.csv'}
+        return f'{data_path}/s3_file.csv'
     
     
-    def _insert_data(file_path, cols):
+    def _insert_data(**kwargs):
         pg_hook = PostgresHook(
             postgres_conn_id=pg_car_conn
         )
-        
-        # file_path = ti.xcom_pull(key='file_path')
-        print(file_path)
-        with open(file_path) as f:
+        file_path = kwargs['templates_dict']['file_path']
+        cols = kwargs['templates_dict']['cols']
+        table_cols = kwargs['templates_dict']['table_cols']
+        with open(file_path, newline='') as f:
             content = csv.DictReader(f)
-        for row in content:
-            vals = itemgetter(cols)(row)
-            sql = f"""
-            INSERT INTO car_data 
-            VALUES ({','.join(vals)})
-                """
-            pg_hook.run(sql)
+            for row in content:
+                print(row, type(row))
+                vals = itemgetter(*cols)(row)
+                sql = f"""
+                INSERT INTO car_data ({','.join(table_cols)})
+                VALUES ('{"','".join([x.strip() if isinstance(x, str) else x for x in vals])}')
+                    """
+                pg_hook.run(sql)
 
     insert_data = PythonOperator(
         task_id='insert_data',
         python_callable=_insert_data,
         provide_context=True,
-        op_kwargs={'file_path':'{{ ti.xcom_pull(task_ids="get_data_from_s3") }}',
-                    'cols':cols}
+        # op_kwargs={'cols':cols},
+        templates_dict={'file_path':'{{ ti.xcom_pull(task_ids="get_data_from_s3") }}',
+                        'cols':cols,
+                        'table_cols':table_cols}
     )
     
     # create_db >> 
-    create_table >> get_data_from_s3(bucket_name=bucket_name,
+    drop_table >> create_table >> get_data_from_s3(bucket_name=bucket_name,
                                      file_key=file_key, 
                                      data_path=DATA_PATH) >>\
                                      insert_data
